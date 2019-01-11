ed154b931bde41de82012e05a0c7bd6f7d0d7643
Kuan-Ying Fang <kuanyingfang@gmail.com>
2019-01-11 12:42:15 -0800

    exploring caching

--- app.py
+++ app.py
@@ -28,6 +28,19 @@ def before_request():
         assert request.method == method
 
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+    
+
 @app.route('/queue', methods = ['GET', 'POST', 'DELETE', 'PUT'])
 def view_queue():
 
